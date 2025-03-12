3103d9726ddff5fb5f091e7eaf921a012782621e
Sagar Khatri <khatrisagar2@gmail.com>
2025-03-12 17:19:01 +0530

    Radio ANd Checkbox

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class HomeState extends StatefulWidget{
 
@@ -12,6 +13,13 @@ class HomeState extends StatefulWidget{
 }
 
 class HomeMain extends State<HomeState>{
+
+  int iGroupValue = 4;
+  bool androidCheck = false;
+  bool iosCheck = false;
+  bool flutterCheck = false;
+  bool reactCheck = false;
+  List<String> technologyArray = [];
 
   @override
   Widget build(BuildContext context) {
@@ -105,11 +113,207 @@ class HomeMain extends State<HomeState>{
                   ),
                 ],
               ),
-            )
+            ),
+            Row(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: Row(
+                    children: [
+                      Radio(
+                        value: 0, 
+                        groupValue: iGroupValue, 
+                        onChanged: (value){                    
+                          setState(() {
+                            setGender(0,"Male");
+                          });
+                        }
+                      ),
+                      Text(
+                        "Male",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Row(
+                    children: [
+                      Radio(
+                        value: 1, 
+                        groupValue: iGroupValue, 
+                        onChanged: (value){                    
+                          setState(() {
+                            setGender(1,"Female");
+                          });
+                        }
+                      ),
+                      Text(
+                        "Female",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Row(
+                    children: [
+                      Radio(
+                        value: 2, 
+                        groupValue: iGroupValue, 
+                        onChanged: (value){                    
+                          setState(() {
+                            setGender(2,"Transgender");
+                          });
+                        }
+                      ),
+                      Text(
+                        "Transgender",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Checkbox(
+                  value: androidCheck, 
+                  onChanged: (value){
+                    setState(() {
+                      androidCheck = value!;
+                      technologyMethod(value,"Android");
+                    });
+                  },
+                ),
+                Text(
+                  "Android",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold
+                  ),
+                )
+              ],
+            ),
+            Row(
+              children: [
+                Checkbox(
+                  value: iosCheck, 
+                  onChanged: (value){
+                    setState(() {
+                      iosCheck = value!;
+                      technologyMethod(value,"iOS");
+                    });
+                  },
+                ),
+                Text(
+                  "iOS",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold
+                  ),
+                )
+              ],
+            ),
+            Row(
+              children: [
+                Checkbox(
+                  value: flutterCheck, 
+                  onChanged: (value){
+                    setState(() {
+                      flutterCheck = value!;
+                      technologyMethod(value,"Flutter");
+                    });
+                  },
+                ),
+                Text(
+                  "Flutter",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold
+                  ),
+                )
+              ],
+            ),
+            Row(
+              children: [
+                Checkbox(
+                  value: reactCheck, 
+                  onChanged: (value){
+                    setState(() {
+                      reactCheck = value!;
+                      technologyMethod(value,"React Native");
+                    });
+                  },
+                ),
+                Text(
+                  "React Native",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold
+                  ),
+                )
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0),
+              child: Container(
+                width: 200.0,
+                height: 40.0,
+                color: Colors.brown.shade400,
+                child: TextButton(
+                  onPressed: (){
+                    if(technologyArray.length>0){
+                      print(technologyArray);
+                    }
+                  }, 
+                  child: Text(
+                    "Show Technology",
+                    style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold,fontSize: 20.0),),
+                ),
+              ),
+            ),
           ],
         ),
       ),
     );
+  }
+
+  void setGender(value,message){
+    iGroupValue = value;
+    Fluttertoast.showToast(
+      msg: message,
+      toastLength: Toast.LENGTH_LONG,
+      gravity: ToastGravity.BOTTOM  
+    );
+  }
+
+  technologyMethod(isSelect,technology){
+    if(isSelect){
+      technologyArray.add(technology);
+    }
+    else{
+      technologyArray.removeWhere((element) => element == technology);
+    }
   }
 
 }
